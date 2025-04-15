26865accd47c39417ff7d5e827cea50953ee26f8
中島卓哉 <>
2025-04-16 02:51:20 +0900

    4321改修終了

--- public/js/user_search.js
+++ public/js/user_search.js
@@ -1,6 +1,9 @@
 $(function () {
   $('.search_conditions').click(function () {
     $('.search_conditions_inner').slideToggle();
+
+    // 矢印の方向を切り替え・追加
+    $(this).find('.arrow-toggle').toggleClass('down');
   });
 
   $('.subject_edit_btn').click(function () {
